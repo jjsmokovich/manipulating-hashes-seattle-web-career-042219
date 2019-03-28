0e2f213a008c06d127e31a036394ddd17a3243a3
Jesse Smokovich <jessesmokovich@yahoo.com>
2019-03-28 06:23:30 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,20 +1,2 @@
-def first_challenge
-  contacts = {
-    "Jon Snow" => {
-      name: "Jon",
-      email: "jon_snow@thewall.we", 
-      favorite_icecream_flavors: ["chocolate", "vanilla", "mint chip"],
-      knows: nil
-    },
-    "Freddy Mercury" => {
-      name: "Freddy",
-      email: "freddy@mercury.com",
-      favorite_icecream_flavors: ["strawberry", "cookie dough", "mint chip"]
-    }
-  }
-
-  
-  contacts.delete_if {|k, v| if v == "Strawberry"}
-  contacts
-end
+.delete_if
 
