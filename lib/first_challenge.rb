e0daafb88f49cd1ebafb38d1f022feda104f6a4b
Jesse Smokovich <jessesmokovich@yahoo.com>
2019-03-28 06:41:32 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -13,9 +13,7 @@ def first_challenge
     }
   }
   
-   contacts["Freddy Mercury"].each do |attribute, value|
-      if attribute == :favorite_icecream_flavors
-        
+   contacts["Freddy Mercury"].each {|attribute, value| if attribute ==  :favorite_icecream_flavors}
         value.delete_if {|flavor| flavor == "strawberry"}
     end
   end
